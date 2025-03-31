25c8cb771a180d4bdfd8a0b85a701dc2a36f5232
Lukasz Motyl <lukasz.motyl5@gmail.com>
2025-03-31 14:56:18 +0200

    adding Post

--- sconmag/src/Controller/Admin/PanelController.php
+++ sconmag/src/Controller/Admin/PanelController.php
@@ -2,6 +2,16 @@
 
 namespace App\Controller\Admin;
 
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
 class PanelController extends AbstractController
 {
+
+    #[Route('/admin')]
+    public function index(): Response
+    {
+
+    }
+
 }
